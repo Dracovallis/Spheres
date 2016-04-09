272b93e4f26f61a6db6a0ed323a98ce0989ac16e
Dracovallis <k.jordanov@mail.bg>
2016-04-09 11:49:00 +0300

    Added some EnemySpawn logic and NPC moving logic

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,11 +4,16 @@ import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Main extends Application {
 
@@ -23,16 +28,26 @@ public class Main extends Application {
     private static final double SCREEN_WIDTH = 1024;
     private static final double SCREEN_HEIGHT = 768;
     private static final double VELOCITY = 3;
-    private static double INITIAL_SCALE = 0.1;
+    private static final double ENEMY_VELOCITY = 1;
+    private static double INITIAL_SCALE = 0.3;
     private static double SCALE_INCREASE_FACTOR = 0.05;
-    private static final String HERO_IMAGE_LOC = "sun.png";
+    private static final String HERO_IMAGE_LOC = "player.png";
     private static final String BACKGROUND_IMAGE_LOC = "starsBackground.jpg";
+    private static final String ENEMIES_IMAGE_LOC = "enemies.png";
+    private static final String FRIENDS_IMAGE_LOC = "friends.png";
+    private static int spawnEnemiesCounter = 0;
 
     private Image heroImage;
     private Image backgroundImage;
 
     private ImageView hero;
     private ImageView background;
+
+    private Image enemyImage;
+    private Image friendImage;
+    private ImageView enemy;
+    private List<ImageView> enemyList = new ArrayList<>();
+
 
     boolean running;
     boolean goNorth;
@@ -57,6 +72,7 @@ public class Main extends Application {
         Group arena = new Group(background);
         arena.getChildren().add(hero);
 
+
         moveHeroTo(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
 
         Scene scene = new Scene(arena, SCREEN_WIDTH, SCREEN_HEIGHT);
@@ -80,6 +96,10 @@ public class Main extends Application {
                     case SHIFT:
                         running = true;
                         break;
+                    case O:
+                        INITIAL_SCALE += 0.1;
+                        break;
+
                 }
             }
         });
@@ -111,9 +131,17 @@ public class Main extends Application {
         stage.show();
 
         AnimationTimer timer = new AnimationTimer() {
-
             @Override
             public void handle(long now) {
+
+                spawnEnemiesCounter++;
+                double heroCurrentScale = INITIAL_SCALE;
+
+                if (spawnEnemiesCounter == 100) {
+                    spawnEnemiesCounter = 0;
+                    spawnEnemy(arena, heroCurrentScale, enemyList);
+                }
+
                 int dx = 0;
                 int dy = 0;
                 hero.setScaleX(INITIAL_SCALE);
@@ -138,9 +166,37 @@ public class Main extends Application {
                     }
                 }
                 moveHeroBy(dx, dy);
+                moveEnemies(enemyList, ENEMY_VELOCITY);
             }
         };
         timer.start();
+
+    }
+
+    private void moveEnemies(List<ImageView> enemyList, double enemySpeed) {
+        double destinationX = hero.getLayoutX();
+        double destinationY = hero.getLayoutY();
+
+        for (ImageView imageView : enemyList) {
+            double enemyCurrentPosX = imageView.getLayoutX();
+            double enemyCurrentPosY = imageView.getLayoutY();
+
+            if (enemyCurrentPosX >= destinationX) {
+                enemyCurrentPosX -= enemySpeed;
+                imageView.relocate(enemyCurrentPosX, enemyCurrentPosY);
+            } else {
+                enemyCurrentPosX += enemySpeed;
+                imageView.relocate(enemyCurrentPosX, enemyCurrentPosY);
+            }
+
+            if (enemyCurrentPosY >= destinationY) {
+                enemyCurrentPosY -= enemySpeed;
+                imageView.relocate(enemyCurrentPosX, enemyCurrentPosY);
+            } else {
+                enemyCurrentPosY += enemySpeed;
+                imageView.relocate(enemyCurrentPosX, enemyCurrentPosY);
+            }
+        }
     }
 
     private void moveHeroBy(int directionX, int directionY) {
@@ -161,12 +217,37 @@ public class Main extends Application {
         final double currentX = hero.getBoundsInLocal().getWidth() / 2;
         final double currentY = hero.getBoundsInLocal().getHeight() / 2;
 
-        if (destinationX - currentX >= 0 &&
-                destinationX + currentX <= SCREEN_WIDTH &&
-                destinationY - currentY >= 0 &&
-                destinationY + currentY <= SCREEN_HEIGHT) {
+        if (destinationX > 0 &&
+                destinationX < SCREEN_WIDTH &&
+                destinationY > 0 &&
+                destinationY < SCREEN_HEIGHT) {
             hero.relocate(destinationX - currentX, destinationY - currentY);
         }
+    }
+
+    private void spawnEnemy(Group arena, double heroCurrentScale, List<ImageView> enemyList) {
+
+        Random r = new Random();
+        double randomScaleFactor = r.nextInt(3) * 0.1;
+
+        if (r.nextInt(2) == 1) {
+            enemyImage = new Image(ENEMIES_IMAGE_LOC);
+            enemy = new ImageView(enemyImage);
+            enemy.setScaleX(heroCurrentScale + randomScaleFactor);
+            enemy.setScaleY(heroCurrentScale + randomScaleFactor);
+            arena.getChildren().add(enemy);
+            enemyList.add(enemy);
+
+        } else {
+            friendImage = new Image(FRIENDS_IMAGE_LOC);
+            enemy = new ImageView(friendImage);
+            enemy.setScaleX(heroCurrentScale - randomScaleFactor);
+            enemy.setScaleY(heroCurrentScale - randomScaleFactor);
+            arena.getChildren().add(enemy);
+            enemyList.add(enemy);
+        }
+
+
     }
 
     public static void main(String[] args) {
